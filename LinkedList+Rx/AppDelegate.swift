02516047011443da8ee6cdc560ce58b9b4aad349
Miguel <gannasong@gmail.com>
2018-07-23 14:42:16 +0800

    add: Initialization

--- LinkedList+Rx/AppDelegate.swift
+++ LinkedList+Rx/AppDelegate.swift
@@ -13,9 +13,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
-
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
+    let rootViewcontroller = ViewController()
+    let navigationController = UINavigationController(rootViewController: rootViewcontroller)
+    window = UIWindow(frame: UIScreen.main.bounds)
+    window?.rootViewController = navigationController
+    window?.makeKeyAndVisible()
+    window?.backgroundColor = UIColor.yellow
     return true
   }
 
